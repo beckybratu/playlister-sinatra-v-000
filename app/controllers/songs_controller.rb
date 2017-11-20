59e34dae5cf56e6cfc73fcbad76f0056f4a8a72a
Rebecca Bratu <beckybratu@gmail.com>
2017-11-20 04:10:55 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,4 +9,23 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
+  post '/songs' do
+    @song = Song.create(:name => params[:name], :artist => params[:artist], :genre => params[:genre])
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+  # patch '/songs/:slug' do
+  #   @song = Song.find_by_slug(params[:slug])
+  #   @song.name = params[:name]
+  #   @song.artist.name = params[:artist.name]
+  #   @song.genre.name = params[:genre.name]
+  #   @song.save
+  #   erb :'songs/show'
+  # end
+
 end
