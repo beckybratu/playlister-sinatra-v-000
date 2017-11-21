c8c7b91c8cc04a0c6001eded199bc1fd6a40ff59
Rebecca Bratu <beckybratu@gmail.com>
2017-11-21 04:44:47 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+
+ use Rack::Flash
 
   get '/songs' do
    @songs = Song.all
@@ -9,23 +13,31 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
-  post '/songs' do
-    @song = Song.create(:name => params[:name], :artist_name => params[:artist_name], :genre_name => params[:genre_name])
-    redirect to "/songs/#{@song.slug}"
-  end
-
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
 
-  # patch '/songs/:slug' do
-  #   @song = Song.find_by_slug(params[:slug])
-  #   @song.name = params[:name]
-  #   @song.artist.name = params[:artist.name]
-  #   @song.genre.name = params[:genre.name]
-  #   @song.save
-  #   erb :'songs/show'
-  # end
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 end
